9a5b8caa89b8f76ed2fe1a9117956e8eb7487fb7
Varadpatil21 <96022723+Varadpatil21@users.noreply.github.com>
2022-10-10 15:18:19 +0530

    Add files via upload

---
+++ Assignment_1.R
@@ -0,0 +1,8 @@
+#data Frame
+#Assignment 1
+Name<-c("Rahul","Vijay","Viraj","Rajesh","Vinay","Abhilash","Rohit","Sujay","Boby","Shubham")
+Mobile_No<-c("860564999","850456999","760564888","9658522852","8596421387","7852296324","8621459756","7569423894","7525631810","7682413589")
+Physics<-c(95,85,40,72,66,48,70,93,50,60)
+Maths<-c(90,92,70,75,96,63,99,72,79,85)
+Student_data<-data.frame(Name,Mobile_No,Physics,Maths)
+print(Student_data)
